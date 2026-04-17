e1c929c19985540a08c35986680ce329652e7812
caohoa <caohoavo22@gmail.com>
2026-04-17 14:55:58 +0800

    Upload environment.prod.ts to git

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  luv2ShopApiUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51T5iauRDzoDAVfvV4JoV5TSMFv82JQFroK0IFU9ZC7pzFGra2NyKc165ihjegWkFNLL1MxfKndnbX83ruY0eNs3z00t4D2qqBX"
+};
