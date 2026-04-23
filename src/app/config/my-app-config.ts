08fe05beea4de39269e4d739c32660ee8c04c58e
caohoa <caohoavo22@gmail.com>
2026-04-23 14:45:28 +0800

    Update login redirectUri for production

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,8 @@ export default {
     oidc: {
         clientId: "0oazbhtt9k78rgNqz697",
         issuer: "https://integrator-5512784.okta.com/oauth2/default",
-        redirectUri: "https://localhost:4200/login/callback",
+        //redirectUri: "https://localhost:4200/login/callback",
+        redirectUri: "https://161.118.221.253:443/login/callback",
         scopes: ['openid', 'profile', 'email']
     }
 }
